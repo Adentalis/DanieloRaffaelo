f12c329e9fa57335134ddaf2fbea44ae9b85fca6
Drogo <drogo@Khal.speedport.ip>
2020-06-25 11:57:55 +0100

    add EnglishPage

---
+++ src/views/EnglishPage/EnglishPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+import Button from "@material-ui/core/Button";
+
+export default class EnglishPage extends Component {
+ 
+  render() {
+    return (
+        <Button> Ich bin english!</Button>
+    );
+  }
+}
