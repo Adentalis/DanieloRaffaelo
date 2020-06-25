76e662612283b18f8bd4edb24290b62f9e81d432
Daniel <daniel.n96@t-online.de>
2020-06-26 01:27:51 +0200

    init google-spreadsheets

--- src/views/VocabularyPage/VocabularyPage.js
+++ src/views/VocabularyPage/VocabularyPage.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 
 import Button from "@material-ui/core/Button";
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+const creds = require("./client_secret.json");
+const doc = new GoogleSpreadsheet(
+  "1IcwMfyNgp2ZdvEbSQPqsXGo2JZG6BbF3dI8xVKdNlgY"
+);
 
 export default class VocabularyPage extends Component {
- 
+  constructor(props) {
+    super(props);
+    this.state = {
+      sheet: 0,
+    };
+    this.accessSpreadsheet = this.accessSpreadsheet.bind(this);
+  }
+
+  async accessSpreadsheet() {
+    await doc.useServiceAccountAuth({
+      client_email: creds.client_email,
+      private_key: creds.private_key,
+    });
+
+    await doc.loadInfo(); // loads document properties and worksheets
+    console.log(doc.title);
+    const sheet = doc.sheetsByIndex[1]; // or use doc.sheetsById[id]
+    console.log(sheet.title);
+  }
+
   render() {
-    return (
-        <Button>Ich bin english!</Button>
-    );
+    return <Button onClick={this.accessSpreadsheet}>Ich bin english!</Button>;
   }
 }
