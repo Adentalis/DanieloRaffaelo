21be5a610c446cca8dab9d025d3e061e70fd5142
Drogo <drogo@Khal.speedport.ip>
2020-06-25 08:17:51 +0100

    display Timer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
+import Times from './views/Components/Times';
 import Components from "./views/Components/Components.js";
 import Header from "./components/Header/Header.js";
 
-import "./App.css";
+import './App.css';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      breakLength: Number.parseInt(this.props.defaultBreakLength, 10),
+      sessionLength: Number.parseInt(this.props.defaultSessionLength, 10),
+      timeLabel: 'Session',
+      timeLeftInSecond: Number.parseInt(this.props.defaultSessionLength, 10) * 60,
+      isStart: false,
+      timerInterval: null
+    }
+
   }
   render() {
     return (
       <div className="App">
-        <Header startText="Hihihihi" />
-        <Components />
+              <Header startText='Hihihihi'/>
+      <Components />
+      <Times
+          timeLabel={this.state.timeLabel}
+          timeLeftInSecond={this.state.timeLeftInSecond}
+        />
       </div>
     );
   }
+
 }
