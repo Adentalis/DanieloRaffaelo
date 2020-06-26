030b1f212acc4b12357db99d0c78c573e88a73fb
Daniel <daniel.n96@t-online.de>
2020-06-26 09:40:57 +0200

    show items in a list

--- src/views/VocabularyPage/VocabularyPage.js
+++ src/views/VocabularyPage/VocabularyPage.js
@@ -1,11 +1,178 @@
 import React, { Component } from "react";
 
 import Button from "@material-ui/core/Button";
+import ObjectList from "react-object-list";
+//import { FontAwesome } from "react-object-list/icons";
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 const creds = require("./client_secret.json");
 const doc = new GoogleSpreadsheet(
   "1IcwMfyNgp2ZdvEbSQPqsXGo2JZG6BbF3dI8xVKdNlgY"
 );
+
+const mockData = [];
+mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});mockData.push({
+  email: "hans22@ff",
+  gender:"fff",
+  first_name:"fd",
+  last_name:" gg"
+});
+
+
 
 export default class VocabularyPage extends Component {
   constructor(props) {
@@ -14,6 +181,7 @@ export default class VocabularyPage extends Component {
       sheet: 0,
     };
     this.accessSpreadsheet = this.accessSpreadsheet.bind(this);
+    this.logState = this.logState.bind(this);
   }
 
   async accessSpreadsheet() {
@@ -28,7 +196,45 @@ export default class VocabularyPage extends Component {
     console.log(sheet.title);
   }
 
+  logState() {
+    console.log(this.state);
+  }
+
   render() {
-    return <Button onClick={this.accessSpreadsheet}>Ich bin english!</Button>;
+    return (
+      <div>
+        <Button onClick={this.accessSpreadsheet}>
+          Ich werde english können!
+        </Button>
+        <Button onClick={this.logState}>State</Button>
+        <ObjectList
+          icons={{
+            OptionalFields: <i className="list" />,
+            Favourites: <i className="heart" />,
+            RemoveFavourite: <i className="trash" />,
+            RemoveFilter: <i className="minus-circle" />,
+            DropdownOpen: <i className="caret-down" />,
+            DropdownClose: <i className="caret-up" />,
+            SortAsc: <i className="caret-up" />,
+            SortDesc: <i className="caret-down" />,
+            Unsorted: <i className="sort" />,
+            Loading: <i className="circle-notch spin" />,
+            CheckboxChecked: <i className="check-square" />,
+            CheckboxUnchecked: <i className="square" />,
+          }}
+          columns={[
+            [
+              { dataKey: "first_name", header: "First Name" },
+              { dataKey: "last_name", header: "Last Name" },
+            ],
+            { dataKey: "email", header: "Email" },
+            { dataKey: "gender", header: "Gender" },
+          ]}
+          data={mockData}
+          favouritesEnabled={false}
+        />
+        ,
+      </div>
+    );
   }
 }
