65115e23330631c3d31c25717f0c967ea25a5879
Daniel <daniel.n96@t-online.de>
2020-08-11 16:30:00 +0200

    add Item

--- src/views/ToDoListPage/ToDoListPage.js
+++ src/views/ToDoListPage/ToDoListPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ToDoList from "./ToDoList";
 
 export default class ToDoListPage extends Component {
   constructor(props) {
@@ -6,24 +7,55 @@ export default class ToDoListPage extends Component {
 
     this.state = { items: [], text: "" };
     this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleAddItem = this.handleAddItem.bind(this);
   }
-  
+
   handleTextChange(event) {
     this.setState({
       text: event.target.value,
     });
   }
 
+  handleAddItem(event) {
+    //event.preventDefault();
+
+    let newItem = {
+      date: new Date().toLocaleDateString(),
+      text: this.state.text,
+      finished: false,
+    };
+
+    this.setState((prevState) => ({
+      items: prevState.items.concat(newItem),
+      text: "",
+    }));
+  }
+
   render() {
     return (
       <div>
         Es gibt viel zu tun Mylord
+        <br />
+        Der lange
+        <br />
+        <ToDoList
+          items={this.state.items}
+          onItemCompleted={this.markItemCompleted}
+          onDeleteItem={this.handleDeleteItem}
+        />
+        <br />
         <input
           type="text"
-          className="form-control"
           onChange={this.handleTextChange}
           value={this.state.text}
         />
+        <button onClick={this.handleAddItem} disabled={!this.state.text}>
+          Jeeey
+        </button>
+        <br />
+        ----------TESTS---------------
+        <br />
+        items --> {this.state.items.length}
       </div>
     );
   }
