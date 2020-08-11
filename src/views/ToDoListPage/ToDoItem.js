cfd72ed1109a2c2c56e90d592bdf60d58f09826c
Daniel <daniel.n96@t-online.de>
2020-08-11 16:29:54 +0200

    add Todo classes

---
+++ src/views/ToDoListPage/ToDoItem.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+class TodoItem extends Component {
+  constructor(props) {
+    super(props);
+    this.markCompleted = this.markCompleted.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+  markCompleted(event) {
+    this.props.onItemCompleted(this.props.id);
+  }
+  deleteItem(event) {
+    this.props.onDeleteItem(this.props.id);
+  }
+
+  render() {
+    return (
+      <li ref={(li) => (this._listItem = li)}>
+        <label>
+          <input type="checkbox" onChange={this.markCompleted} />{" "}
+          {this.props.text}
+        </label>
+        <button type="button" onClick={this.deleteItem}>
+          x
+        </button>
+      </li>
+    );
+  }
+}
