7540eab5f147277a5cfa16ec988b64a41cab6fdb
Drogo <drogo@Khal.speedport.ip>
2020-08-18 10:26:36 +0100

    remove svg

--- src/views/TestPage/Sections/AnimatedCircle/AnimatedCircle.js
+++ src/views/TestPage/Sections/AnimatedCircle/AnimatedCircle.js
@@ -6,29 +6,6 @@ export default class AnimatedCircle extends Component {
     return (
       <div className="animatedCircle">
         <div className="circle"></div>
-        <div className="circle"></div>
-        <svg>
-          <filter id="waves">
-            <feTurbulence
-              x="0"
-              y="0"
-              baseFrequency="0.009"
-              numOctaves="5"
-              seed="2"
-               >
-              <animate
-                attributeName="baseFrequency"
-                dur="60s"
-                values="0.02;0.005;0.02"
-                repeatCount="indefinite"
-              ></animate>
-            </feTurbulence>
-            <feDisplacementMap
-              in="SourceFraphic"
-              scale="30"
-            ></feDisplacementMap>
-          </filter>
-        </svg>
       </div>
     );
   }
