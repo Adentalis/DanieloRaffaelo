03b909f3b691b9730216fb933d25591e7f442558
Drogo <drogo@Khal.speedport.ip>
2020-08-18 17:49:47 +0100

    add first responsive gridlayout

--- src/views/GridPage/GridPage.js
+++ src/views/GridPage/GridPage.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-//import "./styles.css";
+import "./styles.css";
 
 export default class GridPage extends Component {
   render() {
     return (
       <div className="GridPage">
-        Grid
+        <div>A</div> 
+        <div>B</div>
+        <div>C</div>
+        <div>D</div>
+        <div>E</div>
+        <div>F</div>
+        <div>G</div>
       </div>
     );
   }
