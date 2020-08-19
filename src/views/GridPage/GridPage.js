42c469aeecedf672bc509cea6d9f1a69c5cca4da
Drogo <drogo@Khal.speedport.ip>
2020-08-19 12:27:56 +0100

    show current window size

--- src/views/GridPage/GridPage.js
+++ src/views/GridPage/GridPage.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import "./styles.css";
+import Data from "./Sections/Data/Data.js";
+
 
 export default class GridPage extends Component {
   render() {
     return (
       <div className="GridPage">
+        <Data></Data>
         <div>A</div> 
         <div>B</div>
         <div>C</div>
---
+++ src/views/GridPage/Sections/Data/Data.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export default class Data extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0,
+      height: 0,
+    };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+
+  render() {
+    return (
+      <div id="windowData">
+        Das weiß ich über mich
+        <button onClick={this.updateWindowDimensions}>ReloadData</button>
+        <div>W-Height: {this.state.width}</div>
+        <div>W-Width: {this.state.height}</div>
+      </div>
+    );
+  }
+}
