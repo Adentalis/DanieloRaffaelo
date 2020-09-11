9ec612cbdd782f01ca24de1e57b197fb57731f41
Daniel <daniel.n96@t-online.de>
2020-09-11 10:48:02 +0200

    add first 10 worlist img's

--- src/views/MnemoPage/MnemoPage.js
+++ src/views/MnemoPage/MnemoPage.js
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
+import "./styles.css";
 
 export default class MnemoPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      images: "",
+      nr: 0,
+    };
+    this.changeImg = this.changeImg.bind(this);
+  }
+
+  componentWillMount() {
+    const images = this.importAll(
+      require.context("../../assets/img/wordlist/", false, /\.(png|jpe?g|svg)$/)
+    );
+    this.setState({ images });
+    var nr = Math.floor(Math.random() * this.state.images.length);
+    this.setState({ nr });
+  }
+
+  importAll(r) {
+    return r.keys().map(r);
+  }
+
+  changeImg() {
+    var nr = Math.floor(Math.random() * this.state.images.length);
+    this.setState({ nr });
   }
 
   render() {
-    return <div className="mnemo-page">in Bearbeitung :)</div>;
+    return (
+      <div className="mnemo-page">
+        <img
+          id="hihi"
+          onClick={this.changeImg}
+          src={this.state.images[this.state.nr]}
+        ></img>
+      </div>
+    );
   }
 }
