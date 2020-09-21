1977a40c823e9df7344e3a4feb4600bb349b393f
Daniel <daniel.n96@t-online.de>
2020-09-21 15:07:23 +0200

    remove github cat

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,20 +2,10 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
-import Folkme from "../Folkme/Folkme.js";
 
 export default function Header() {
   return (
     <AppBar position="sticky">
-      <Folkme
-        targetURL=""
-        color="#4c4d4e"
-        backgroundColor="#ffafff"
-        position="right"
-        size="140px"
-        ariaLabel="View source on Github"
-      />
-
       <Toolbar id="back-to-top-anchor">
         <Button href="/"> Danielus Raffaellus</Button>
         <Button href="/projects"> Projekte </Button>
