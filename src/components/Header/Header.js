51b11adc231dead368629a5e07276c6c2a2abc45
Daniel <daniel.n96@t-online.de>
2020-09-23 18:01:10 +0200

    create own Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,34 @@
 import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
+import "./Header.css";
 
 export default function Header() {
   return (
-    <AppBar position="sticky">
-      <Toolbar id="back-to-top-anchor">
-        <Button href="/"> Danielus Raffaellus</Button>
-        <Button href="/projects"> Projekte </Button>
-      </Toolbar>
-    </AppBar>
+    <div className="header">
+      <a href="/" className="header-logo">
+        Adentalis
+      </a>
+      <div className="header-toggle"></div>
+      <ul>
+        <li>
+          <a href="/"> Home</a>
+        </li>
+        <li>
+          <a href="/projects"> Projekte</a>
+        </li>
+        <li>
+          <a href="/test"> Test</a>
+        </li>
+      </ul>
+    </div>
   );
 }
+// import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import Button from "@material-ui/core/Button";
+
+// <AppBar position="sticky">
+//   <Toolbar id="back-to-top-anchor">
+//     <Button href="/"> Danielus Raffaellus</Button>
+//     <Button href="/projects"> Projekte </Button>
+//   </Toolbar>
+// </AppBar>
